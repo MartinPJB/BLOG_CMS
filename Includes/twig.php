fa3b77c4ebf2a0f4068f2027fce339e427524510
Cheeto <martin.bronnimann.pj@gmail.com>
2023-11-16 17:06:50 +0100

    Récupération des templates (A FAIRE: STYLES)

--- Includes/twig.php
+++ Includes/twig.php
@@ -5,9 +5,6 @@ function init_twig($directory, $theme)
 {
     $loader = new \Twig\Loader\FilesystemLoader("$directory/Themes/$theme/templates");
 
-    // Ajout du dossier public à Twig
-    $loader->addPath("$directory/Themes/$theme/public", 'public');
-
     $twig = new \Twig\Environment($loader, ['debug' => true]);
     $twig->addExtension(new \Twig\Extension\DebugExtension());
 
