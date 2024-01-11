600300b27d651d34e2c081bcf024721e2d4cab4d
Mael Garrigues <maelgarrigues@gmx.com>
2024-01-11 15:23:57 +0100

    Fixed progress bar on small articles

--- Themes/cuej/Front/public/js/_reading_progress.js
+++ Themes/cuej/Front/public/js/_reading_progress.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
   const article = document.getElementById("article");
   const articleProgress = document.getElementById("article-progress");
   const yFixer = (85 * window.innerHeight) / 100;
-  window.addEventListener("scroll", function() {
-    let scrollPosition = window.scrollY - yFixer;
-    let articleHeight = article.clientHeight;
-    let windowHeight = window.innerHeight;
+  if (article.offsetHeight > 1.25*window.innerHeight) {
+    window.addEventListener("scroll", function() {
+      let scrollPosition = window.scrollY - yFixer;
+      let articleHeight = article.clientHeight;
+      let windowHeight = window.innerHeight;
 
-    let progress = (scrollPosition / (articleHeight - windowHeight)) * 100;
+      let progress = (scrollPosition / (articleHeight - windowHeight)) * 100;
 
-    articleProgress.style.width = progress + "%";
-  });
+      articleProgress.style.width = progress + "%";
+    });
+  } else {
+    articleProgress.style.width = "100%";
+  }
 });
